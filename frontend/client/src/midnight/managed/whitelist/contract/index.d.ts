09a0e9c90f7366ecd8fb13a7e410535dee2a498d
Olatide Bali <obali3@uwo.ca>
2026-05-16 02:51:57 -0400

    Frontend connected with The Midnight DApp Connector and The Midnight Smart Contract & Cryptography pipeline(no testing)

---
+++ frontend/client/src/midnight/managed/whitelist/contract/index.d.ts
@@ -0,0 +1,78 @@
+import type * as __compactRuntime from '@midnight-ntwrk/compact-runtime';
+
+export type Witnesses<PS> = {
+  local_secret_key(context: __compactRuntime.WitnessContext<Ledger, PS>): [PS, Uint8Array];
+  find_whitelist_path(context: __compactRuntime.WitnessContext<Ledger, PS>,
+                      pk_0: Uint8Array): [PS, { leaf: Uint8Array,
+                                                path: { sibling: { field: bigint
+                                                                 },
+                                                        goes_left: boolean
+                                                      }[]
+                                              }];
+}
+
+export type ImpureCircuits<PS> = {
+  add_to_whitelist(context: __compactRuntime.CircuitContext<PS>,
+                   pk_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  verify_whitelist_membership(context: __compactRuntime.CircuitContext<PS>,
+                              report_hash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type ProvableCircuits<PS> = {
+  add_to_whitelist(context: __compactRuntime.CircuitContext<PS>,
+                   pk_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  verify_whitelist_membership(context: __compactRuntime.CircuitContext<PS>,
+                              report_hash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type PureCircuits = {
+}
+
+export type Circuits<PS> = {
+  add_to_whitelist(context: __compactRuntime.CircuitContext<PS>,
+                   pk_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  verify_whitelist_membership(context: __compactRuntime.CircuitContext<PS>,
+                              report_hash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type Ledger = {
+  whitelist: {
+    isFull(): boolean;
+    checkRoot(rt_0: { field: bigint }): boolean;
+    root(): __compactRuntime.MerkleTreeDigest;
+    firstFree(): bigint;
+    pathForLeaf(index_0: bigint, leaf_0: Uint8Array): __compactRuntime.MerkleTreePath<Uint8Array>;
+    findPathForLeaf(leaf_0: Uint8Array): __compactRuntime.MerkleTreePath<Uint8Array> | undefined;
+    history(): Iterator<__compactRuntime.MerkleTreeDigest>
+  };
+  spent_nullifiers: {
+    isEmpty(): boolean;
+    size(): bigint;
+    member(elem_0: Uint8Array): boolean;
+    [Symbol.iterator](): Iterator<Uint8Array>
+  };
+  reports: {
+    isEmpty(): boolean;
+    size(): bigint;
+    member(key_0: Uint8Array): boolean;
+    lookup(key_0: Uint8Array): Uint8Array;
+    [Symbol.iterator](): Iterator<[Uint8Array, Uint8Array]>
+  };
+  readonly verification_count: bigint;
+}
+
+export type ContractReferenceLocations = any;
+
+export declare const contractReferenceLocations : ContractReferenceLocations;
+
+export declare class Contract<PS = any, W extends Witnesses<PS> = Witnesses<PS>> {
+  witnesses: W;
+  circuits: Circuits<PS>;
+  impureCircuits: ImpureCircuits<PS>;
+  provableCircuits: ProvableCircuits<PS>;
+  constructor(witnesses: W);
+  initialState(context: __compactRuntime.ConstructorContext<PS>): __compactRuntime.ConstructorResult<PS>;
+}
+
+export declare function ledger(state: __compactRuntime.StateValue | __compactRuntime.ChargedState): Ledger;
+export declare const pureCircuits: PureCircuits;
